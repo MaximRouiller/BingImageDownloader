1e938045866c21f7a8407e45693ce6128a1321eb
Maxime Rouiller <angelzerosoft@gmail.com>
2017-10-12 08:35:07 -0400

    Simple background downloader because I'm lazy

---
+++ src/BingImageDownloader.Client/Model/BingResult.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BingImageDownloader.Model
+{
+    public partial class BingResult
+    {
+        [JsonProperty("images")]
+        public Image[] Images { get; set; }        
+    }
+
+    public partial class Image
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }        
+    }
+}
